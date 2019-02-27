a1e20cec3aaa6972d4c3c9007349357bc527a0d8
Dhiren_Patel <dhiren887@gmail.com>
2019-02-27 15:17:55 +0530

    Add bootstrep and homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Create A blog models
+class blog(models.Model):
+	title=models.CharField(max_length=255)
+	pub_date=models.DateTimeField()
+	body=models.TextField()
+	image=models.ImageField(upload_to='images/')
+
+
+
+
+# create migration
+
+
+
+#migrate
+
+# add to admin
