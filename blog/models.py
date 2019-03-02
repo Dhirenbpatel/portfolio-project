8796b3dac64eef9d5315a14794fa1d9b2645fad8
Dhiren_Patel <dhiren887@gmail.com>
2019-03-02 11:38:47 +0530

    blog completion and polish

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,12 @@ class blog(models.Model):
 	body=models.TextField()
 	image=models.ImageField(upload_to='images/')
 
-
-
+	def __str__(self):
+		return self.title
+	def summary(self):
+		return self.body[:100]
+	def pub_dates(self):
+		return self.pub_date.strftime('%b %e %Y')
 
 # create migration
 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -122,6 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS=[
+            os.path.join(BASE_DIR, 'portfolio/static')
+]
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
